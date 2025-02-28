6226dbbb78bbd5669dab412add72827817ad5f8e
Daniel Parente <daniel2437@hotmail.com>
2025-02-28 13:23:39 -0300

    adicao da pasta de componentes

---
+++ agro-market-react/src/pages/Home/index.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import Navbar from "../../components/navbar";
+import "../../styles/app.css";
+
+function Home() {
+  return (
+    <>
+      <Navbar/>
+      
+    </>
+  );
+}
+
+export default Home;
--- agro-market-react/tailwind.config.js
+++ agro-market-react/tailwind.config.js
@@ -1,11 +1,12 @@
+// filepath: /home/user/Desktop/AgroMarket2/FIAP_AgroMKT/agro-market-react/tailwind.config.js
 /** @type {import('tailwindcss').Config} */
 export default {
-    content: [
-      "./index.html",
-      "./src/**/*.{js,ts,jsx,tsx}",
-    ],
-    theme: {
-      extend: {},
-    },
-    plugins: [],
-  }
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
